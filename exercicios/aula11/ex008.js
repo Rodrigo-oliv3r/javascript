7020f771fe5a320ebc3ee9c10e8e8facf4ca172d
Rodrigo de Oliveira Peres <oliveira.dev00@gmail.com>
2024-12-28 11:20:45 -0400

    exercício sobre condições

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel =59
+
+console.log(`A valecidade do seu carro é de ${vel}km\h`)
+
+if (vel > 60 ){ //condição simples
+    console.log(`Você utrapassou o limite de velociade. Multado!`)
+}
+
+console.log(`Dirija sempre com o cinto de segurança.`)
+
