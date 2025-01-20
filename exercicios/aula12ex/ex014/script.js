e325eeea4cd8957da2d2a8a9b4e8244525045577
Rodrigo de Oliveira Peres <oliveira.dev00@gmail.com>
2025-01-20 16:32:52 -0400

    Exercício de JavaScript

--- exercicios/aula12ex/ex014/script.js
+++ exercicios/aula12ex/ex014/script.js
@@ -1,20 +1,51 @@
-function carregar(){
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
+function verificar(){
     var data = new Date()
-    var hora = data.getHours()
-    var min = data.getMinutes()
-  
-    msg.innerHTML = `Agora é exatamente ${hora} horas e ${min} minutos.`
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
 
-    if(hora >= 0 && hora < 12){
-        img.scr = 'imagens/manha-250.png'
-        document.body.style.background = '#cfa65f'
-    }else if(hora >= 12 && hora < 18){
-        img.src = 'imagens/tarde-250.png'
-        document.body.style.background = '#7f451d'
-    }else{
-        img.src = 'imagens/noite-250.png'
-        document.body.style.background = '#293347'
+    if (fano.value.length  == 0 || Number(fano.value)>ano){
+        window.alert('[ERRO] Verifique os dados e tente  novamente!') 
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if(fsex[0].checked) {
+            genero = 'Homem'
+            if(idade  >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'imagens/bebe-menino.png')
+            }else if(idade < 21){
+                // jovem
+                img.setAttribute('scr', 'imagens/homen-joven.png')
+            }else if (idade <50 ){
+                // adulto
+                img.setAttribute('src', 'imagens/homem-adulto.png')
+            }else {
+                // idoso
+                img.setAttribute('src', 'imagens/idoso.png')
+            }
+        }else if(fsex[1].checked){
+            genero = 'Mulher'
+            if(idade  >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'imagens/bebe-menina.png') 
+            }else if(idade < 21){
+                // jovem
+                img.setAttribute('src', 'imagens/mulher-joven.png')
+            }else if (idade <50 ){
+                // adulto
+                img.setAttribute('src', 'imagens/mulher-adulta.png')
+            }else {
+                // idoso
+                img.setAttribute('src', 'imagens/idosa.png')
+            }
+        }
+
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
